d4cdcb72dd3fbc9f20e71b936d36f315b435db39
jaekw <jae-kwen@daum.net>
2025-08-12 11:14:05 +0900

    feature: project 생성

---
+++ src/main/java/com/jproject/weeCloudService/WeeCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jproject.weeCloudService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeCloudServiceApplication.class, args);
+	}
+
+}
